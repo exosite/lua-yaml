c3bf357e98e25593761ab799da87dfc3b2e7e2d9
PavelK <2me@pavelk.ru>
2023-02-01 22:05:52 +0300

    fix(liblua): Numbers 123mb fix(liblua): Inline arrah/hash comments

--- samples/comments.lua
+++ samples/comments.lua
@@ -8,5 +8,13 @@ return {
   ["key7"] = {
     ["key8"] = "value",
     ["key9"] = "not#comments"
-  }
+  },
+  ["key10"] = {1, 2},
+  ["key11"] = "[3, 4] it is another string",
+  ["key12"] = "[5, 6] StrinG",
+  ["key13"] = {7, 8},
+  ["key14"] = "[9, 10] StrinG",
+  ["key15"] = "[11, 12] StrinG# notcomment",
+  ["key16"] = {13, 14},
+  ["key17"] = "[15, 16] string"
 }
